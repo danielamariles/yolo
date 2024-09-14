32828474951e3e27137c8381055068c895985676
danielamariles <amarilesdaniela@gmail.com>
2024-09-14 17:48:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
 )
 
 # Main page heading
-st.title("Object Detection And Tracking using YOLOv8")
+st.title("Proyecto feria de la ciencia -Detección de objetos - Isabella Amariles & Celeste Garcia")
 
 # Sidebar
 st.sidebar.header("ML Model Config")
