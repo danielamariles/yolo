76d1994d495cd9465012fc6f388f3163171517f8
danielamariles <amarilesdaniela@gmail.com>
2024-09-14 20:59:50 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -28,6 +28,7 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
 VIDEOS_DICT = {
+    'video_1': VIDEO_DIR / 'video_4.mp4',
     'video_1': VIDEO_DIR / 'video_1.mp4',
     'video_2': VIDEO_DIR / 'video_2.mp4',
     'video_3': VIDEO_DIR / 'video_3.mp4',
